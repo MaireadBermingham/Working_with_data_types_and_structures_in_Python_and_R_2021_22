7cdb0014ec2f77656e15f4bc366fd7bf9ee474d4
MaireadBermingham <mairead.bermingham@ed.ac.uk>
2022-06-05 16:36:27 +0000

    Python notebook update 2

--- RScripts/Using_R_in_Noteable.R
+++ RScripts/Using_R_in_Noteable.R
@@ -10,10 +10,12 @@
 (3 + 7)^2 - 1*10/4
 
 
-#Here we are going to use the c() function to collect data entered at the console.
-#The c() function in R programming stands for combine.
-#We are going to name each collection of data, and then perform a numerical operation to calculate BMI. 
-#In this example we conduct an analysis that is analogous to working in a spreadsheet.
+# Using the c() and cbind() base R functions to combine data values and objects respectively. 
+#Here we are going to use the c() function from the *base* R package to collect data entered at the console. 
+#The c() function in R programming stands for combine. We are then going to name each collection of data and 
+#perform a numerical operation to calculate BMI. We will then use the `cbind()` to combine the Height,  Weight and BMI objects. 
+#The `cbind()` *base* R function combine R objects by rows or columns. 
+#In this example, we conduct an analogous analysis to working in a spreadsheet. 
 
 Height <- c(1.6, 1.8, 2.0, 2.5) # height data in m
 Weight <- c(50, 60, 64, 95)     # weight data in kg
@@ -21,15 +23,19 @@ BMI <-   Weight/(Height^2)      # BMI
 cbind(Height , Weight, BMI) # column bind, like spreadsheet
 
 
-#Using 'readr' from 'tidyverse' to write the BMI data to file
-#'tidyverse' is a collection of essential R packages for data science.
-#'readr' provides a fast and friendly way to read rectangular data from delimited files, such as comma-separated values (CSV) and tab-separated values (TSV). 
-# A data frame is a table or a two-dimensional array-like structure in which each column contains values of one variable and each row contains one set of values from each column.
-#The function data.frame() creates data frames
+# Using readr from tidyverse to write the BMI data to file¶
+# tidyverse is a collection of essential R packages for data science. 
+#readr provides a fast and friendly way to read rectangular data from delimited files, 
+#such as comma-separated values (CSV) and tab-separated values (TSV). 
+#A data frame is a table or a two-dimensional array-like structure in which each column contains 
+# values of one variable and each row contains one set of values from each column. 
+#The function data.frame() creates data frames.
+
+#Load the R packages that you will need to run the next few lines of R code.
 library(tidyverse)
 library(readr)
 
-#Create a data frame from the BMI data
+#Create a data frame from the BMI data.
 Data<-data.frame(cbind(Height , Weight, BMI))
 view(Data)
 head(Data)
